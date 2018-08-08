d17004cbf0ba2ffb0effa0e902d8bd678f1eabe8
Steve Oh <OhxSteve@gmail.com>
2018-08-07 18:48:47 -0700

    Implement put and refactor linter

--- controllers/bookings.js
+++ controllers/bookings.js
@@ -3,9 +3,9 @@ const db = require('../models');
 module.exports = {
   get: (req, res, next) => {
     const roomId = req.params.id;
-    db.Booking.find({ roomId: roomId })
+    db.Booking.find({ roomId })
       .exec()
-      .then(data => {
+      .then((data) => {
         if (!data || !data.length) {
           next();
         } else {
@@ -16,13 +16,21 @@ module.exports = {
       .catch(next);
   },
   post: (req, res, next) => {
-    const newBooking = new db.Booking(req.body);  
-    newBooking.save(err => {  
-        if (err) return res.status(500).send(err);
-        return res.status(200).send(newTodoObj);
+    const newBooking = new db.Booking(req.body);
+    newBooking.save((err) => {
+      if (err) {
+        return res.status(500).send(err);
+      }
+      return res.status(200).send(newTodoObj);
     });
   },
 
+  put: db.Booking.findByIdAndUpdate(req.params.todoId, req.body, { new: true }, (err, todo) => {
+    if (err) {
+      return res.status(500).send(err);
+    }
+    return res.send(todo);
+  }),
 
-  create: function(req, res) {},
+  create(req, res) {},
 };
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,12 @@
-const express = require("express");
-const ctrl = require("./../controllers");
+const express = require('express');
+const ctrl = require('./../controllers');
+
 const router = express.Router();
 
-router.get("/rooms/:id/bookings", ctrl.bookings.get);
+router.get('/rooms/:id/bookings', ctrl.bookings.get);
 
-router.post('/rooms/:id/bookings',ctrl.bookings.post);
+router.post('/rooms/:id/bookings', ctrl.bookings.post);
+
+router.put('/rooms/:id/bookings', ctrl.bookings.put);
 
 module.exports = router;
