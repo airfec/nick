3f70e3d86983eaa7329deae151e6f0c019e5e382
Steve Oh <OhxSteve@gmail.com>
2018-08-09 19:54:53 -0700

    Implement data generation

---
+++ generate-b2.js
@@ -0,0 +1,25 @@
+const fs = require('fs');
+const faker = require('faker');
+
+const createBookingData = () => {
+  const roomID = faker.random.number({
+    min: 1,
+    max: 10000000,
+  });
+  const checkIn = faker.date.between('2018-07-24', '2019-12-31');
+  const duration = faker.random.number({
+    min: 1,
+    max: 31,
+  });
+  const dataItem = `${checkIn},${duration},${roomID}`;
+  return dataItem;
+};
+
+const writeBookingsCSV = () => {
+  const out = fs.createWriteStream('./bookings2.csv');
+  for (let i = 0; i < 25000000; i++) {
+    out.write(`${createBookingData()}\n`);
+  }
+};
+
+writeBookingsCSV();
